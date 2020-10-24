eea1ecab333bd192af664a51691aba404af8b91a
error418 <error418@users.noreply.github.com>
2020-10-24 18:11:52 +0200

    chore(rtd): add master doc reference

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@ from recommonmark.parser import CommonMarkParser
 # for Sphinx-1.4 or newer
 extensions = ['recommonmark']
 
+master_doc = "README.md"
+
 # for Sphinx-1.3
 #from recommonmark.parser import CommonMarkParser
 
