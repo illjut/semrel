113616292d5385d2e1c38cf6ff52eb661be3a655
error418 <error418@users.noreply.github.com>
2020-10-24 18:34:00 +0200

    chore(rtd): set page title

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@ from recommonmark.parser import CommonMarkParser
 # for Sphinx-1.4 or newer
 extensions = ['recommonmark']
 
+project = u'Gradle semantic-release Plugin'
+
 # for Sphinx-1.3
 #from recommonmark.parser import CommonMarkParser
 
