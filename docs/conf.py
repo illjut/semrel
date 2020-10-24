6539db1445ad0f05525a8dc5c0ecc2e9a284116b
error418 <error418@users.noreply.github.com>
2020-10-24 17:37:45 +0200

    chore: add rtd config

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# for MarkdownParser
+from sphinx_markdown_parser.parser import MarkdownParser
+
+def setup(app):
+    app.add_source_suffix('.md', 'markdown')
+    app.add_source_parser(MarkdownParser)
+    app.add_config_value('markdown_parser_config', {
+        'auto_toc_tree_section': 'Content',
+        'enable_auto_doc_ref': True,
+        'enable_auto_toc_tree': True,
+        'enable_eval_rst': True,
+        'extensions': [
+            'extra',
+            'nl2br',
+            'sane_lists',
+            'smarty',
+            'toc',
+            'wikilinks',
+            'pymdownx.arithmatex',
+        ],
+    }, True)
+
+# for CommonMarkParser
+from recommonmark.parser import CommonMarkParser
+
+def setup(app):
+    app.add_source_suffix('.md', 'markdown')
+    app.add_source_parser(CommonMarkParser)
+    app.add_config_value('markdown_parser_config', {
+        'auto_toc_tree_section': 'Content',
+        'enable_auto_doc_ref': True,
+        'enable_auto_toc_tree': True,
+        'enable_eval_rst': True,
+        'enable_inline_math': True,
+        'enable_math': True,
+    }, True)
